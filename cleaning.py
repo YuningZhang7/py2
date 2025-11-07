38f1384a2b7fc37268ccaf4b1a8e213d3232a6a9
georgiasilk <s2890267@ed.ac.uk>
2025-11-07 13:13:43 +0000

    cleaning.py updated

--- cleaning.py
+++ cleaning.py
@@ -1,4 +1,19 @@
 import pandas as pd
+import numpy as np
 data = pd.read_csv('https://assets.publishing.service.gov.uk/media/6762f39cff2c870561bde826/Postcode_level_all_meters_electricity_2023.csv')
-print(data.head())
 #We want to remove the postcodes which are not in scotland
+# List of Scottish postcodes
+scot_postcodes = ['AB', 'DD', 'DG', 'EH', 'FK', 'G', 'HS', 'IV', 'KA', 'KW', 'KY', 'ML', 'PA', 'PH', 'TD', 'ZE']
+# Copy list so we don't delete the original
+clean_data = data.copy()
+# Ensures that all Outcodes as type string with no spaces around the edges and are uppercase
+clean_data['Outcode'] = clean_data['Outcode'].astype(str).str.strip().str.upper()
+# Keep only the rows with Scottish postcodes
+clean_data = clean_data[clean_data['Outcode'].str.startswith(tuple(scot_postcodes))]
+clean_data = clean_data[~clean_data['Outcode'].str.startswith(('GU', 'GL'))]
+# Print the starts of outcodes and how many postcodes are within each one
+print(clean_data['Outcode'].str.extract(r'^([A-Z]{1,2})')[0].value_counts())
+# Print the length of the clean data
+print(f"Remaining rows: {len(clean_data)}")
+# Export data to csv
+clean_data.to_csv('electricity_scotland_clean_2023.csv', index=False)
