3982a1401fa5e85bd803c4a74cf91114aa6d58c2
vb-04 <varshabadri4@gmail.com>
2025-11-20 14:41:43 +0000

    modified code for housing dataset

--- HouseholdDataClean.py
+++ HouseholdDataClean.py
@@ -4,5 +4,6 @@ house_data = pd.read_csv('DataZoneHouseholdData.csv')
 clean_house_data = house_data.copy()
 columns_to_remove = ['All occupied households', 'Whole house or bungalow: Detached', 'Whole house or bungalow: Semi-detached', 'Whole house or bungalow: Terraced (including end-terrace)', 'Flat, maisonette or apartment: Purpose-built block of flats or tenement', 'Flat, maisonette or apartment: Part of a converted or shared house (including bed-sits)', 'Flat, maisonette or apartment: In a commercial building']
 
-for col in columns_to_remove[col]:
-    clean_house_data.drop(col, inplace=True, axis=1)
+#clean_house_data = clean_house_data.drop(columns_to_remove,axis=1)
+
+print(clean_house_data.columns)
